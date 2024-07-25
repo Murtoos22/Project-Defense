c9f8efdc3b885cceaed1cef79b55da226272fa3c
Andrey Spassov <aspassov75@gmail.com>
2024-07-25 14:27:02 +0300

    added base project layout and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import Login from '../components/login/Login';
+import Register from '../components/register/Register';
+import Home from '../components/home/Home';
+
 function App() {
   return (
     <>
-      <h1>Hello React Project</h1>
+      <Header />
+      
+      <main className="main-container-box">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </main>
+
+      <Footer />
     </>
   );
 };
